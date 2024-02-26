58371c3b706b7b4bd83aa366c970c5278c8dcd96
yassixe <e.yassinelk@gmail.com>
2024-02-25 23:25:09 -0500

    get_mode

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <efilib.h>
 
 
+
+
+
+
 EFI_STATUS
 EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
@@ -19,8 +23,14 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
   Print(L"year:%d\nmounth:%d\nday:%d\nhour:%d\n",current_time.Year,current_time.Month,current_time.Day,current_time.Hour);
   //let's try to get memory mapping.
   //
-  unsigned long val;
-  asm volatile("mov %%cr0, %0" : "=r" (val));
-  Print(L"cr0: 0x%x\n",val);
+  // unsigned long val;
+  // asm volatile("mov %%cr0, %0" : "=r" (val));
+  // Print(L"cr0: 0x%x\n",val);
+  uefi_call_wrapper(ST->ConOut->Reset,1,ST->ConOut);
+  uefi_call_wrapper(ST->ConOut->ClearScreen,1,ST->ConOut);
+  UINTN mode_id = (UINTN)(ST->ConOut->Mode->Mode);
+  UINTN col,lig;
+  uefi_call_wrapper(ST->ConOut->QueryMode,4,ST->ConOut,mode_id,&col,&lig);
+  Print(L"mode:%d = %d*%d\n",ST->ConOut->Mode->Mode,lig,col);
   return EFI_SUCCESS;
 }
